275bb1d2bac5036adb4f15b1f5687574f3136f59
tqcenglish <tqcenglish@gmail.com>
2013-04-28 15:13:29 +0800

    test rm 01

--- 第一部分_底层网络/01/client/Test.py
+++
@@ -1,8 +0,0 @@
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print ("Looking up port number ...")
-port = socket.getservbyname('http','tcp')
-print port
-s.connect(('www.google.com.hk', port))
-print s.getsockname()
-print s.getpeername()
--- 第一部分_底层网络/01/client/client02.py
+++
@@ -1,9 +0,0 @@
-'''
-Created on 2013-4-26
-
-@author: miracle
-'''
-import urllib, sys
-f = urllib.urlopen('http://www.baidu.com:80')
-for line in f.readlines():
-    sys.stdout.write(line)
